0b2c514a977bdc5797e04ab4eb189faae2a78126
Aman Patel <amanofficially02@gmail.com>
2025-03-31 21:13:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ tailwind.config = {
                 Ovo: ["Ovo", "serif"]
             },
             animation:{
-                spin_slow: 'spin 3s linear infinite'
+                spin_slow: 'spin 7s linear infinite'
             },
             colors:{
                 lightHover: '#fcf4ff',
